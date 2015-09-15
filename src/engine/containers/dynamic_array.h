7d59a5a928e3ec6fee0097d8aa9b2597337ff425
Barbara Crépeau <barbara.crepeau@gmail.com>
2015-09-14 22:20:27 -0400

    [DynArray] Reorganize + Add rule of three + operator <<

--- src/engine/containers/dynamic_array.h
+++ src/engine/containers/dynamic_array.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_DYNAMIC_ARRAY
 # define INCLUDE_DYNAMIC_ARRAY
 
+#include <stdexcept>
 #include "../memory/iallocator.h"
 
 namespace StevensDev
@@ -29,6 +30,8 @@ class DynamicArray
 
 
     // MEMBER FUNCTIONS
+    // These functions are private because we don't want them to be used in a
+    // bad way, outside the class.
 
     void            shiftRight( unsigned int index );
       // Shift the array to the right, starting at index value
@@ -42,58 +45,186 @@ class DynamicArray
 
   public:
 
-    // CONSTRUCTOR
+    // CONSTRUCTORS
 
     DynamicArray( IAllocator<T>* alloc );
       // Pointer to an allocator for use with memory
 
+    DynamicArray( const DynamicArray<T>& copy );
+      // Copy constructor
 
-    // ACCESSORS
 
-    unsigned int const getLength();
-      // Number of elements contained
+    // DESTRUCTOR
 
-    const T         at( unsigned int index );
-      // Retrieves an element at a location, throws if out of bounds
+    virtual             ~DynamicArray();
 
 
     // OPERATORS
 
-    T               operator[]( int index );
+    DynamicArray&       operator=( const DynamicArray& allocator );
+      // Copy assignment operator
+
+    T&                  operator[]( int index );
       // Sets an element, undefined behavior if out of bounds
 
-    T               operator[]( int index ) const;
+    T                   operator[]( int index ) const;
       // Retrieves an element, undefined behaviour if out of bounds
+
+
+    // ACCESSORS
+
+    unsigned int const  getLength();
+      // Number of elements contained
+
+    const T             at( unsigned int index );
+      // Retrieves an element at a location, throws if out of bounds
 
 
     // MEMBER FUNCTIONS
 
-    void            push( const T& element);
+    void                push( const T& element );
       // Adds element to end of collection, grow
 
-    void            pushFront( T element );
+    void                pushFront( T element );
       // Pushes to the front of the collection, grow, shift
 
-    T               pop();
+    T                   pop();
       // Removes and retrieve the last element, shifts
 
-    T               popFront();
+    T                   popFront();
       // Removes and retrieve the first element, shifts
 
-    T               removeAt( unsigned int index );
+    T                   removeAt( unsigned int index );
       // Removes, throws if invalid, shifts
 
-    T               insertAt( unsigned int index, const T& element );
+    T                   insertAt( unsigned int index, const T& element );
       // Can grow, shifts
 };
 
+
+// PRIVATE MEMBER FUNCTIONS
+// Declared first because private
+
 template<typename T>
-inline DynamicArray<T>::DynamicArray( sgdm::IAllocator<T>* alloc )
+inline void
+DynamicArray<T>::shiftRight( unsigned int index )
+{
+    // Error checking
+    if ( index < 0 || index > d_length )
+        throw std::out_of_range( "DynamicArray::shiftRight: out of range" );
+
+    // Check if allocated size is enough
+    if (d_length + 1 > d_size)
+        reallocate();
+
+    // Shift to the right
+    for ( int i = d_length; i > index; i-- )
+    {
+        d_array[i] = d_array[i - 1];
+    }
+}
+
+template<typename T>
+inline void
+DynamicArray<T>::shiftLeft( unsigned int index )
+{
+    // Error checking
+    if ( index < 0 || index > d_length )
+        throw std::out_of_range( "DynamicArray::shiftLeft: out of range" );
+
+    // Shift to the left
+    for ( int i = index; i < d_length; i++ )
+    {
+        d_array[i] = d_array[i + 1];
+    }
+}
+
+template<typename T>
+inline void
+DynamicArray<T>::reallocate()
+{
+    unsigned int    new_size = d_size * 2;
+    T*              new_array = d_allocator->get( new_size );
+
+    // Move the values from d_array to the new memory space
+    for ( int i = 0; i < d_length; i++ )
+    {
+        new_array[i] = d_array[i];
+    }
+
+    // Release the old memory space
+    d_allocator->release( d_array, d_size );
+
+    // Update values of the array
+    d_size = new_size;
+    d_array = new_array;
+}
+
+
+// CONSTRUCTORS
+
+template<typename T>
+inline
+DynamicArray<T>::DynamicArray( sgdm::IAllocator<T>* alloc )
     : d_allocator( alloc ), d_array( 0 ), d_length( 0 ), d_size( 0 )
 {
-    d_array = alloc->get(1);
+    d_array = alloc->get( 1 );
     d_size = 1;
 }
+
+template<typename T>
+inline
+DynamicArray<T>::DynamicArray( const DynamicArray<T>& copy )
+    : d_allocator( copy.d_allocator ), d_array( 0 ),
+      d_length( copy.d_length ), d_size( copy.d_size )
+{
+    for ( int i = 0; i < d_length; i++ )
+        d_array[i] = copy.d_array[i];
+}
+
+
+// DESTRUCTOR
+
+template<typename T>
+inline
+DynamicArray<T>::~DynamicArray()
+{
+    d_allocator->release( d_array, d_size );
+}
+
+
+// OPERATORS
+
+template<typename T>
+inline DynamicArray<T>&
+DynamicArray<T>::operator=( const DynamicArray<T>& array )
+{
+    d_allocator = array.d_allocator;
+    d_length = array.d_length;
+    d_size = array.d_size;
+
+    for ( int i = 0; i < d_length; i++ )
+        d_array[i] = array.d_array[i];
+
+    return *this;
+}
+
+template<typename T>
+inline T&
+DynamicArray<T>::operator[]( int index )
+{
+    return d_array[index];
+}
+
+template<typename T>
+inline T
+DynamicArray<T>::operator[]( int index ) const
+{
+    return d_array[index];
+}
+
+
+// ACCESSORS
 
 template<typename T>
 inline unsigned int const DynamicArray<T>::getLength()
@@ -104,23 +235,18 @@ inline unsigned int const DynamicArray<T>::getLength()
 template<typename T>
 inline const T DynamicArray<T>::at( unsigned int index )
 {
-    return *(d_array + index);
-}
+    // Error checking
+    if ( index < 0 || index >= d_length )
+        throw std::out_of_range( "DynamicArray::at: out of range" );
 
-template<typename T>
-inline T DynamicArray<T>::operator[]( int index )
-{
     return d_array[index];
 }
 
-template<typename T>
-T DynamicArray<T>::operator[]( int index ) const
-{
-    return d_array[index];
-}
+
+// MEMBER FUNCTIONS
 
 template<typename T>
-inline void DynamicArray<T>::push( const T& element)
+inline void DynamicArray<T>::push( const T& element )
 {
     insertAt( d_length, element );
 }
@@ -166,7 +292,7 @@ inline T DynamicArray<T>::removeAt( unsigned int index )
 {
     // Error checking
     if ( index < 0 || index > d_length )
-        return 0; // TODO: Throw exception
+        throw std::out_of_range( "DynamicArray::shiftLeft: out of range" );
 
     // Decrease size and shift array
     shiftLeft( index );
@@ -178,7 +304,7 @@ inline T DynamicArray<T>::insertAt( unsigned int index, const T& element )
 {
     // Error checking
     if ( index < 0 || index > d_length )
-        return 0; // TODO: Throw exception
+        throw std::out_of_range( "DynamicArray::shiftLeft: out of range" );
 
     // Allocate new memory if needed
     if ( d_length >= d_size)
@@ -195,57 +321,23 @@ inline T DynamicArray<T>::insertAt( unsigned int index, const T& element )
     return element;
 }
 
-template<typename T>
-inline void DynamicArray<T>::shiftRight( unsigned int index )
+
+// FREE OPERATORS
+
+template<class T>
+inline std::ostream&
+operator<<( std::ostream& stream, const DynamicArray<T> array )
 {
-    // Error checking
-    if ( index < 0 || index > d_length )
-        return; // TODO: Throw exception
+    stream << "{ "
+        << "\"length\": " << array.getLength() << ", "
+        << "\"array\": [ " << array.getLength();
 
-    // Check if allocated size is enough
-    if (d_length + 1 >= d_size)
-        reallocate();
+    for ( int i = 0; i < array.getLength(); i++ )
+        stream << array[i] << ", ";
 
-    // Shift to the right
-    for (int i = d_length; i > index; i--)
-    {
-        d_array[i] = d_array[i - 1];
-    }
+    return stream << " ] }";
 }
 
-template<typename T>
-inline void DynamicArray<T>::shiftLeft( unsigned int index )
-{
-    // Error checking
-    if ( index < 0 || index > d_length )
-        return; // TODO: Throw exception
-
-    // Shift to the left
-    for (int i = index; i < d_length; i++)
-    {
-        d_array[i] = d_array[i + 1];
-    }
-}
-
-template<typename T>
-inline void DynamicArray<T>::reallocate()
-{
-    unsigned int    new_size = d_size * 2;
-    T*              new_array = d_allocator->get(new_size);
-
-    // Move the values from d_array to the new memory space
-    for (int i = 0; i < d_length; i++)
-    {
-        new_array[i] = d_array[i];
-    }
-
-    // Release the old memory space
-    d_allocator->release(d_array, d_size);
-
-    // Update values of the array
-    d_size = new_size;
-    d_array = new_array;
-}
 
 } // End sgdc namespace
 } // End StevensDev namespace
