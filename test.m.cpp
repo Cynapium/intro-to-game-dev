ffae800e62cc78dd7db2572f8ea811e1cbaa5bbd
Cynapium <barbara.crepeau@gmail.com>
2015-09-28 23:50:27 -0400

    [GTest] Fixed tests launch - Forgot to add test main

--- test.m.cpp
+++ test.m.cpp
@@ -1,12 +1,6 @@
 // test.m.cpp
 
 #include <gtest/gtest.h>
-#include "tests/engine/containers/dynamic_array_tests.h"
-#include "tests/engine/containers/map_tests.h"
-#include "tests/engine/memory/default_allocator_tests.h"
-#include "tests/engine/memory/counting_allocator_tests.h"
-
-//#include "tests/engine/data/json_parser.h"
 
 int main( int argc, char **argv )
 {
