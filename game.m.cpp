52d43e8fccb4ce5d41d27eb00b04b1a2935b2d75
Cynapium <barbara.crepeau@gmail.com>
2015-10-12 21:37:02 -0400

    [Main] Update initial position of sprite

--- game.m.cpp
+++ game.m.cpp
@@ -5,6 +5,9 @@
 #include "scene/scene.h"
 #include "input/input.h"
 
+#define WIDTH               800
+#define HEIGHT              500
+
 int main( int argc, char **argv )
 {
     using namespace StevensDev;
@@ -12,7 +15,7 @@ int main( int argc, char **argv )
     // Setup Renderer
     sgdr::Renderer   *renderer = new sgdr::Renderer();
     renderer->loadTexture( "pika", "resources/textures/test.png" );
-    renderer->setupWindow( 500, 500 );
+    renderer->setupWindow( WIDTH, HEIGHT );
 
     // Setup Scene
     sgds::Scene                 scene = sgds::Scene::inst();
@@ -20,7 +23,7 @@ int main( int argc, char **argv )
 
     // Create sprites
     sgdr::RenderableSprite     *sprite = new sgdr::RenderableSprite( renderer->getTexture( "pika" ) );
-    sprite->setPosition( 10, 10 );
+    sprite->setPosition( WIDTH / 2 - 25, HEIGHT / 2 - 25 );
     renderer->addSprite( sprite );
 
     sgdi::Input                 &in = sgdi::Input::inst();
