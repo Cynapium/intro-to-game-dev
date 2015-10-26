d7dc04efde0e0fbeb6e809724b3a985ea40c8c21
Cynapium <barbara.crepeau@gmail.com>
2015-10-26 13:35:27 -0400

    [GTest][Json] Add ctor & dtor to JsonParserTests

--- tests/engine/data/json_parser.cpp
+++ tests/engine/data/json_parser.cpp
@@ -2,3 +2,19 @@
 
 #include "json_parser.h"
 
+JsonParserTest::JsonParserTest()
+{
+}
+
+void JsonParserTest::SetUp()
+{
+}
+
+void JsonParserTest::TearDown()
+{
+}
+
+JsonParserTest::~JsonParserTest()
+{
+}
+
