9f7ecc752050e9449956541e34484dc1a5e64637
Cynapium <barbara.crepeau@gmail.com>
2015-10-29 19:36:01 -0400

    [GTest][Input] Try to resolve travis segfault

--- tests/engine/input/input_tests.h
+++ tests/engine/input/input_tests.h
@@ -11,21 +11,23 @@ class InputTest : public ::testing::Test
   public:
 
     StevensDev::sgdi::Input                 &d_in;
+      // Instance of the Input singleton
 
 
     // CONSTRUCTOR
 
     InputTest();
-    // Set-up work which cannot throw exception for each tests
+      // Set-up work which cannot throw exception for each tests
 
     // DESTRUCTOR
+
     virtual ~InputTest();
 
 
     // MEMBER FUNCTIONS
 
     virtual void SetUp();
-    // Set-up work for each tests
+      // Set-up work for each tests
 
     virtual void TearDown();
       // Clean-up work for each tests
@@ -37,13 +39,10 @@ class InputTest : public ::testing::Test
 TEST_F( InputTest, IsDown )
 {
     #define INPUT_MOUSE( key )                                             \
-        EXPECT_EQ( sf::Mouse::isButtonPressed( sf::Mouse::key ),           \
-                   d_in.isDown( StevensDev::sgdi::InputType::MOUSE_##key ) );
-
+        EXPECT_FALSE( d_in.isDown( StevensDev::sgdi::InputType::MOUSE_##key ) );
 
     #define INPUT_KEY( key )                                               \
-        EXPECT_EQ( sf::Keyboard::isKeyPressed( sf::Keyboard::key ),        \
-                   d_in.isDown( StevensDev::sgdi::InputType::KEY_##key ) );
+        EXPECT_FALSE( d_in.isDown( StevensDev::sgdi::InputType::KEY_##key ) );
 
     #include "input/input_type.def"
 
@@ -57,7 +56,6 @@ TEST_F( InputTest, IsUp )
     #define INPUT_MOUSE( key )                                             \
         EXPECT_EQ( !sf::Mouse::isButtonPressed( sf::Mouse::key ),          \
                    d_in.isUp( StevensDev::sgdi::InputType::MOUSE_##key ) );
-
 
     #define INPUT_KEY( key )                                               \
         EXPECT_EQ( !sf::Keyboard::isKeyPressed( sf::Keyboard::key ),       \
